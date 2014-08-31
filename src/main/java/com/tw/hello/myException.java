7728f369f5057d70aaa85afaa66aadcc5d9e5c92
Tingpeng <robin_1966@163.com>
2014-08-31 21:31:04 +0800

    自定义异常

---
+++ src/main/java/com/tw/hello/myException.java
@@ -0,0 +1,31 @@
+package com.tw.hello;
+
+public class MyException extends Exception{
+    public MyException(){
+        super();
+    }
+    public MyException(String msg){
+        super(msg);
+    }
+    public MyException(String msg, Throwable throwable){
+        super(msg, throwable);
+    }
+    public MyException(Throwable throwable){
+        super(throwable);
+    }
+}
+
+class ExceptionTest {
+    public static void execute(String a) throws MyException {
+        System.out.println("execute...");
+        if("true".equals(a)){
+            throw new MyException("参数不能为 true");
+        }
+    }
+
+    public static void main(String[] args) throws MyException {
+
+        execute("true");
+    }
+}
+
---
+++ src/main/java/com/tw/hello/testException.java
@@ -0,0 +1,21 @@
+package com.tw.hello;
+
+public class TestException {
+    public static boolean test() {
+        try {
+            System.out.println("This is in try block!");
+            return true;
+        } catch (Exception e){
+            System.out.println("This is in catch block!");
+            return false;
+        }finally {
+            System.out.println("This is in finally block!");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean a = test();
+        System.out.println(a);
+    }
+}
